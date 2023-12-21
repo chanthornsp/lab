8dfa63ce88dd8c9fc2566b6af13fa1831ea279ae
chanthornsp <hoeurnchanthorn@gmail.com>
2023-12-21 15:28:06 +0700

    route and controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function user()
+    {
+        return [
+            'name' => 'John Doe',
+            'email' => 'example@email.com'
+        ];
+    }
+}
